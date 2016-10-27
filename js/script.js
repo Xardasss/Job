cce6adf67ff85f17a7d8242c6d434bf5e6f7c7d9
Xardasss <pasha_20.12.0802@mail.ru>
2016-10-27 15:28:46 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-            var BrowserDetect = { 
+  var BrowserDetect = { 
   init: function () { 
   this.browser = this.searchString(this.dataBrowser) || "An unknown browser"; 
   this.version = this.searchVersion(navigator.userAgent) || this.searchVersion(navigator.appVersion) || "an unknown version"; 
